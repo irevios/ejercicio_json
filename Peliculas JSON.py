a61695540b64fe7af182d43301771fb8a885a88d
irevios <irevios@gmail.com>
2018-03-07 18:17:07 +0100

    Archivo Python

---
+++ Peliculas JSON.py
@@ -0,0 +1,23 @@
+import json
+
+def duracion_formato(duracion):
+    duracion = int(duracion.replace('PT','').replace('M',''))
+    hora = int(duracion/60)
+    min = duracion - hora*60
+    if min == 0:
+        if hora == 1:
+            return str(hora) + " hora"
+        return str(hora) + " horas"
+    return str(hora) + " horas y " + str(min) + " minutos"
+
+def listar_peliculas(arbol):
+    lista = []
+    for i in range(len(arbol)):
+        lista.append((arbol[i]["title"],arbol[i]["year"], duracion_formato(arbol[i]["duration"])))
+
+    return lista
+
+with open("movies.json") as fichero:
+    arbol = json.load(fichero)
+
+listar_peliculas(arbol)
